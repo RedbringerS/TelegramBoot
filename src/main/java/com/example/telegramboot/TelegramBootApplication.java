ee4fcfe87d4786f5216102a7249fc1886cbfef79
Ivan Lastouski <redbringers@gmail.com>
2022-06-20 21:44:23 +0300

    commit

---
+++ src/main/java/com/example/telegramboot/TelegramBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegramboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegramboot/TelegramBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegramboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
